966b42b5e132578c5efaca80a79a4097006cb7d8
Mr. Delowar <contact.delowar@gmail.com>
2021-12-02 08:13:19 +0600

    improved

--- js/core.js
+++ js/core.js
@@ -6,18 +6,23 @@ var dhikrs = [
 
 var dhikr = dhikrs[Math.floor(Math.random() * dhikrs.length)]
 
-
-
-
-var showNotification = {
-    type: 'basic',
-    iconUrl: 'icon/icon48.png',
-    title: dhikr,
-    message: "Wet your tounge with the remembrance of Allah"
-}
-
-chrome.notifications.create('remindDhikr',showNotification);
-
 $("#notifyMe").html(dhikr)
 
+function notifyUser()
+{
+    var showNotification = {
+        type: 'basic',
+        iconUrl: 'icon/icon48.png',
+        title: dhikr,
+        message: "Wet your tounge with the remembrance of Allah"
+    }
+    
+    const d = new Date();
+    let time = d.getTime()
+    remindDhikr = 'dhikr'+time
+    chrome.notifications.create(remindDhikr,showNotification);
+}
+notifyUser()
+
+setInterval(notifyUser(), 15000);
 // https://dua.gtaf.org/api/en/details/260
