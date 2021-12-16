f9a818ba9712ff4f62d710ad78a4acc2810ac357
Mr. Delowar <contact.delowar@gmail.com>
2021-12-16 12:43:50 +0600

    making some improvement

--- js/core.js
+++ js/core.js
@@ -1,13 +1,25 @@
-var dhikrId = Math.floor(Math.random() * 328) + 1;
+var dhikrId = Math.floor(Math.random() * 1) + 1;
 
-$("#notifyMe").html(dhikr)
+var notificationData = gtafs[dhikrId];
+
+// console.log(notificationData)
+$('.title').html(notificationData.pageTitle)
+$('.top').html(notificationData.details[0].top)
+$('.arabic').html(notificationData.details[0].arabic)
+$('.translation').html(notificationData.details[0].translation)
+$('.bottom').html(notificationData.details[0].bottom)
+$('.reference').html(notificationData.details[0].reference)
+
+ramandaCounter()
+
+// notifyUser()
 
 function notifyUser()
 {
     var showNotification = {
         type: 'basic',
         iconUrl: 'icon/icon48.png',
-        title: dhikr,
+        title: 'dsfdsf',
         message: "Wet your tounge with the remembrance of Allah"
     }
     
@@ -18,3 +30,25 @@ function notifyUser()
 }
 
 // https://dua.gtaf.org/api/en/details/260
+
+// Ramadan Reminder
+function ramandaCounter()
+{
+  // Set the date we're counting down to
+var countDownDate = new Date("Apr 1, 2022 17:00:00").getTime();
+// Update the count down every 1 second
+var x = setInterval(function() {
+  // Get today's date and time
+  var now = new Date().getTime();
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  // Display the result in the element with id="demo"
+  $("#demo").html(days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ");
+}, 1000);
+}
---
+++ js/gtaf.js
@@ -0,0 +1,44 @@
+var gtafs = [
+    {
+        "details": [
+            {
+                "book_id": "1",
+                "dua_global_id": "2",
+                "ID": "1.1",
+                "dua_segment_id": "",
+                "top": "",
+                "arabic_diacless": "ألحمد للٰه الذي أحيانا بعد ما أماتنا، وإليه النشور",
+                "arabic": "أَلْحَمْدُ لِلّٰهِ الَّذِيْ أَحْيَانَا بَعْدَ مَا أَمَاتَنَا، وَإِلَيْهِ النُّشُوْرُ",
+                "transliteration": "Alhamdu lillaahil-lathee 'ahyaanaa ba'da maa 'amaatanaa wa'ilayhin-nushoor",
+                "translations": "All praise is for Allaah who gave us life after having taken it from us and unto Him is the resurrection",
+                "bottom": "",
+                "reference": "Al-Bukhari, cf. Al-Asqalani, Fathul-Bari 11/113; Muslim 4/2083",
+                "app_reference": "2:1:80:11,2:2:48:80,2:1:80:9,2:1:80:21,2:1:80:22,2:1:97:23,2:1:97:24,2:4:43:277,2:6:34:54"
+            }
+        ],
+        "pageTitle": "When waking up #1",
+        "hasAudio": true,
+        "audio_id": 1
+    },
+    {
+        "details": [
+            {
+                "book_id": "1",
+                "dua_global_id": "3",
+                "ID": "1.2",
+                "dua_segment_id": "",
+                "top": "",
+                "arabic_diacless": "لا إلٰه إلا اللٰه وحده لا شريك له، له الملك وله الحمد، وهو على كل شيء قدير، سبحان اللٰه، والحمد للٰه، ولا إلٰه إلا اللٰه، واللٰه أكبر، ولا حول ولا قوة إلا باللٰه العلي العظيم، رب اغفر لي",
+                "arabic": "لَا إِلٰهَ إِلاَّ اللّٰهُ وَحْدَهُ لَا شَرِيْكَ لَهُ، لَهُ الْمُلْكُ وَلَهُ الْحَمْدُ، وَهُوَ عَلَى كُلِّ شَيْءٍ قَدِيْرٌ، سُبْحَانَ اللّٰهِ، وَالْحَمْدُ لِلّٰهِ، وَلَا إِلٰهَ إِلَّا اللّٰهُ، وَاللّٰهُ أَكْبَرُ، وَلَا حَوْلَ وَلَا قُوَّةَ إِلَّا بِاللّٰهِ الْعَلِيِّ الْعَظِيْمِ، رَبِّ اغْفِرْ لِيْ",
+                "transliteration": "Laa 'illaha 'illallahu wahdahu la shareeka lahu, lahul-mulku wa lahul-hamdu, wa Huwa 'alaa kulli shay'in Qadeer Subhaanallahi, walhamdu lillaahi, wa laa 'ilaha 'illallahu, wallaahu 'akbar, wa laa hawla wa laa Quwwata 'illaa billaahil-'Aliyyil-'Adheem, Rabbighfir lee.",
+                "translations": "None has the right to be worshipped except Allaah, alone, without any partner, to Him belong sovereignty and praise and He is over all things wholly capable. How perfect Allaah is, and all praise is for Allaah, and none has the right to be worshipped except Allaah, Allaah is the greatest and there is no power nor might except with Allaah, The Most High, The Supreme, O my Lord forgive me.",
+                "bottom": "Whoever says this will be forgiven, and if he supplicates Allah, his prayer will be answered; if he performs ablution and prays, his prayer will be accepted.",
+                "reference": "Al-Bukhari, cf. Al-Asqalani, Fathul-Bari 3/39, among others. The wording here is from Ibn Majah 2/335.",
+                "app_reference": "2:1:19:35,2:6:34:52,2:5:48:45"
+            }
+        ],
+        "pageTitle": "When waking up #2",
+        "hasAudio": true,
+        "audio_id": 2
+    }
+]
